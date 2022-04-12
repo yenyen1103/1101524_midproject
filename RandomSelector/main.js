d9a3532d8a03742805fc708951d69b6e0640a1f8
yenyen1103 <tzuyen1103@gmail.com>
2022-04-13 03:52:24 +0800

    Hello

---
+++ RandomSelector/main.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+    //document.write("Hello JavaScript");
+};
+
+$(function(){
+    $("input").on("click",function(){
+        alert("Hi");
+    });
+});
+
+$(function(){
+    $("input").on("click",function(){
+        //alert("Hi");
+        $("h1").text("Hello");
+    });
+});
+
+$(function(){
+    $("input").on("click",function(){
+        //alert("Hi");
+        $("h1").text($("li:first").text());
+    });
+});
+
+$(function(){
+    $("input").on("click",function(){
+        //alert("Hi");
+        $("h1").text($("li:last").text());
+    });
+});
+
+$(function(){
+    $("input").on("click",function(){
+        //alert("Hi");
+        $("h1").text($("li").eq(1).text());
+    });
+});
+
+$(function(){
+    $("input").on("click",function(){
+        //alert("Hi");
+        var numberOfListItem = $("li").length;
+        var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+        $("h1").text($("li").eq(randomChildNumber).text());
+    });
+});
---
+++ YouTube心情點唱機/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "SwZMS-ISbH4",
+    "CKSQjJDQTaQ",
+    "Si5pQHRRH5w"
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [101,109],
+    [126,142],
+    [98,108]
+    ];
